0a685cddb5974165294ba01081b046ae11bfb4e1
KarlaHerrera15 <162621653+KarlaHerrera15@users.noreply.github.com>
2025-04-10 08:16:08 +0000

    punto 4

--- email_herrera/src/app/app.component.ts
+++ email_herrera/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Email } from '../Email/email.models'
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  emailvett:Email[] = [];
+    invia(email: HTMLInputElement, oggetto: HTMLInputElement, testo: HTMLInputElement): boolean {
+      console.log(email.value, oggetto.value,testo.value)
+      this.emailvett.push(new Email (email.value, oggetto.value,testo.value));
+    return false;
+    }
+  
 }
